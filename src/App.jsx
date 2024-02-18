20db0984fb043514867aba447bd8c5182fa451fe
aay7ush <aay7ush@gmail.com>
2024-02-18 16:03:10 +0530

    refactor: remove layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Layout from './components/Layout'
-import CountryDetails from './pages/CountryDetails'
+import CountryDetail from './pages/CountryDetail'
 import CountryListings from './pages/CountryListings'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<CountryListings />} />
-          <Route path="/:countryName" element={<CountryDetails />} />
-        </Route>
+        <Route path="/" element={<CountryListings />} />
+        <Route path="/:countryName" element={<CountryDetail />} />
       </Routes>
     </BrowserRouter>
   )
